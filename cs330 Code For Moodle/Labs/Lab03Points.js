cd5baa16c18de99d87f42ea13235da4423cc4662
ojhscottsmith <ojhscottsmith@gmail.com>
2024-09-10 09:50:11 -0400

    Lab3 changes

--- cs330 Code For Moodle/Labs/Lab03Points.js
+++ cs330 Code For Moodle/Labs/Lab03Points.js
@@ -3,46 +3,50 @@ var gl;
 var points;
 init();
 
-function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+function init() {
+  var canvas = document.getElementById("gl-canvas");
 
-    gl = canvas.getContext('webgl2');
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-    
-    points=[
-      vec2(   ,   ),
-      vec2(   ,   ),
-      vec2(   ,   )
-    ];
-    
-    //
-    //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+  gl = canvas.getContext("webgl2");
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
 
-    //  Load shaders and initialize attribute buffers
+  points = [
+    vec2(1, -1),
+    vec2(-0.5, 1),
+    vec2(-1, 0.5),
+    vec2(-0.25, -0.75),
+    vec2(0.25, 0.75),
+    vec2(1, 0.25),
+  ];
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+  //
+  //  Configure WebGL
+  //
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    // Load the data into the GPU
+  //  Load shaders and initialize attribute buffers
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
 
-    // Associate out shader variables with our data buffer
+  // Load the data into the GPU
 
-    var positionLoc = gl.getAttribLocation( program, "aPosition" );
-    gl.vertexAttribPointer( positionLoc , 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( positionLoc );
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    render();
-};
+  // Associate out shader variables with our data buffer
+
+  var positionLoc = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
+
+  render();
+}
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.POINTS, 0, points.length );
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, points.length);
 }
