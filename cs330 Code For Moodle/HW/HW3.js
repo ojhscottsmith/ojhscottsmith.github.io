fa7e27a634a83338ed8a2775e3f82398b0e9b333
ojhscottsmith <ojhscottsmith@gmail.com>
2024-09-26 09:59:46 -0400

    HW3

--- cs330 Code For Moodle/HW/HW3.js
+++ cs330 Code For Moodle/HW/HW3.js
@@ -5,70 +5,128 @@ Computer Graphics
 */
 "use strict";
 
-var canvas;
 var gl;
 
-var maxNumPositions = 200;
+var morph = true;
 
-var save = false;
+var canvas;
 
-var t;
-var points = [vec2(0.0, 0.0)];
+var color = vec4(0.0, 0.0, 1.0, 1.0);
+var colorLoc;
+
+var L;
+
+var V;
 
 init();
 
 function init() {
-  canvas = document.getElementById("gl-canvas");
+  var canvas = document.getElementById("gl-canvas");
 
   gl = canvas.getContext("webgl2");
   if (!gl) alert("WebGL 2.0 isn't available");
 
-  // 2 - listener event for the button which toggles whether to save the points clicked
-  var saveWork = document.getElementById("Button1");
-  saveWork.addEventListener("click", function () {
-    saveWork = !saveWork;
-    //if (!saveWork) points=[vec2(  0.00 ,  0.00 )];
-    points = [vec2(0.0, 0.0)];
-    render();
-  });
-
-  // 1 - listener event for the mouse click
-  // 3 - will later need to be updated in conjunction with whether the points are saved
-  canvas.addEventListener("mousedown", function (event) {
-    if (!saveWork) points = [vec2(0.0, 0.0)];
-    t = vec2(
-      (2 * event.clientX) / canvas.width - 1,
-      (2 * (canvas.height - event.clientY)) / canvas.height - 1
-    );
-    points.push(t);
-    render();
-  });
-
+  //
+  //  Configure WebGL
+  //
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  //
+
   //  Load shaders and initialize attribute buffers
-  //
+
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
-  var bufferId = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-  gl.bufferData(gl.ARRAY_BUFFER, 8 * Math.pow(3, 6), gl.STATIC_DRAW);
-  var postionLoc = gl.getAttribLocation(program, "aPosition");
-  gl.vertexAttribPointer(postionLoc, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(postionLoc);
+  var L = [
+    vec2(-0.5, 0.8),
+    vec2(-0.5, -0.8),
+
+    vec2(-0.5, -0.8),
+    vec2(0.5, -0.8),
+
+    vec2(0.5, -0.8),
+    vec2(0.5, -0.5),
+
+    vec2(0.5, -0.5),
+    vec2(-0.2, -0.5),
+
+    vec2(-0.2, -0.5),
+    vec2(-0.2, 0.8),
+
+    vec2(-0.2, 0.8),
+    vec2(-0.5, 0.8),
+  ];
+
+  // Load the data into the GPU
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(L), gl.STATIC_DRAW);
+
+  // Associate out shader variables with our data buffer
+
+  var lpositionLoc = gl.getAttribLocation(program, "lPosition");
+  var vpositionLoc = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(lpositionLoc, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(lpositionLoc);
+
+  var V = [
+    vec2(-0.8, 0.8),
+    vec2(0.0, -0.8),
+
+    vec2(0.0, -0.8),
+    vec2(0.8, 0.8),
+
+    vec2(0.8, 0.8),
+    vec2(0.5, 0.8),
+
+    vec2(0.5, 0.8),
+    vec2(0.0, -0.2),
+
+    vec2(0.0, -0.2),
+    vec2(-0.5, 0.8),
+
+    vec2(-0.8, 0.8),
+    vec2(-0.5, 0.8),
+  ];
+
+  // Load the data into the GPU
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(V), gl.STATIC_DRAW);
+
+  // Associate out shader variables with our data buffer
+
+  var positionLoc = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vpositionLoc, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vpositionLoc);
+
+  thetaLoc = gl.getUniformLocation(program, "t");
+  colorLoc = gl.getUniformLocation(program, "aColor");
+
+  //define the uniform variable in the shader, aColor
+
+  // button listener here, toggle rotation
+  document.getElementById("Morph").onclick = function () {
+    morph = !morph;
+  };
 
   render();
 }
 
 function render() {
-  // replace the points data in the GPU
-  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
-
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // don't render the first point, (0,0)
-  gl.drawArrays(gl.POINTS, 1, points.length - 1);
+  theta += morph ? 0.1 : 0.0;
+
+  gl.uniform1f(thetaLoc, theta);
+
+  gl.uniform4fv(colorLoc, color);
+
+  gl.drawArrays(gl.LINES, 0, V.Length);
+
+  setTimeout(function () {
+    requestAnimationFrame(render);
+  }, delay);
 }
