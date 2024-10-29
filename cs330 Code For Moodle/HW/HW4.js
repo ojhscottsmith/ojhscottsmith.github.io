1c6fdadbd1060675f2716b4aedfd6a3ae65ab889
ojhscottsmith <ojhscottsmith@gmail.com>
2024-10-29 09:24:25 -0400

    HW4

--- cs330 Code For Moodle/HW/HW4.js
+++ cs330 Code For Moodle/HW/HW4.js
@@ -1,26 +1,31 @@
 "use strict";
+
+var canvas;
 var gl;
-var tParam = 0.0;
-var tLoc;
-var deltaT = 0.01;
-var color = vec4(1.0, 0.65, 0.0, 1.0);
+
+var numPositions = 36;
+
 var positions = [];
 var colors = [];
-var numPositions = 36;
-//var vec4 color;
-var Ucolor = vec4(1.0, 0.65, 0.0, 1.0);
-var Icolor = vec4(0.0, 0.0, 1.0, 1.0);
-var colorLoc;
-var delay = 100;
-var morph = true;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
 var axis = 0;
 var theta = [0, 0, 0];
+
 var thetaLoc;
-var rotatenum = 0;
+
 var rotate = false;
+
+var rotatenum = 0;
+
 init();
+
 function init() {
-  var canvas = document.getElementById("gl-canvas");
+  canvas = document.getElementById("gl-canvas");
+
   gl = canvas.getContext("webgl2");
   if (!gl) alert("WebGL 2.0 isn't available");
 
@@ -38,14 +43,15 @@ function init() {
   colors.push(vec4(0.0, 0.0, 1.0, 1.0));
   positions.push(vec4(0.0, 0.0, 1.0, 1.0));
   colors.push(vec4(0.0, 0.0, 1.0, 1.0));
-  //
-  //  Configure WebGL
-  //
+
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
   gl.enable(gl.DEPTH_TEST);
+
+  //
   //  Load shaders and initialize attribute buffers
+  //
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
@@ -53,24 +59,35 @@ function init() {
   gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-  var colorLoc = gl.getAttribLocation(program, "IColor");
+  var colorLoc = gl.getAttribLocation(program, "aColor");
   gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(colorLoc);
 
-  // Load the I into the GPU
-  var vBufferI = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vBufferI);
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
 
-  // Associate out shader variables with our data buffer
-  var ipositionLoc = gl.getAttribLocation(program, "iPosition");
-  gl.vertexAttribPointer(ipositionLoc, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(ipositionLoc);
+  var positionLoc = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
 
   thetaLoc = gl.getUniformLocation(program, "uTheta");
 
+  //event listeners for buttons
+
+  document.getElementById("xButton").onclick = function () {
+    axis = xAxis;
+  };
+  document.getElementById("yButton").onclick = function () {
+    axis = yAxis;
+  };
+  document.getElementById("zButton").onclick = function () {
+    axis = zAxis;
+  };
+
   render();
 }
+
 function colorCube() {
   quad(1, 0, 3, 2);
   quad(2, 3, 7, 6);
@@ -81,17 +98,15 @@ function colorCube() {
 }
 
 function quad(a, b, c, d) {
-  //Needs to change to vec4
-  //Vec4(x, y, z, w)
-  var I = [
-    vec4(-0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5, 0.5, 0.5, 1.0),
-    vec4(0.5, 0.5, 0.5, 1.0),
-    vec4(0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5, -0.5, -0.5, 1.0),
-    vec4(-0.5, 0.5, -0.5, 1.0),
-    vec4(0.5, 0.5, -0.5, 1.0),
-    vec4(0.5, -0.5, -0.5, 1.0),
+  var vertices = [
+    vec4(-1.0, -0.25, 0.5, 1.0),
+    vec4(-1.0, 0.25, 0.5, 1.0),
+    vec4(1.0, 0.25, 0.5, 1.0),
+    vec4(1.0, -0.25, 0.5, 1.0),
+    vec4(-1.0, -0.25, 0.0, 1.0),
+    vec4(-1.0, 0.25, 0.0, 1.0),
+    vec4(1.0, 0.25, 0.0, 1.0),
+    vec4(1.0, -0.25, 0.0, 1.0),
   ];
 
   var vertexColors = [
@@ -114,7 +129,7 @@ function quad(a, b, c, d) {
   var indices = [a, b, c, a, c, d];
 
   for (var i = 0; i < indices.length; ++i) {
-    positions.push(I[indices[i]]);
+    positions.push(vertices[indices[i]]);
     colors.push(vertexColors[indices[i]]);
 
     // for solid colored faces use
@@ -132,71 +147,13 @@ function quad(a, b, c, d) {
   };
 }
 
-// var U = [
-//   vec2(-0.75, 0.75),
-//   vec2(-0.38, 0.75),
-//   vec2(-0.38, -0.38),
-//   vec2(0.38, -0.38),
-//   vec2(0.38, 0.75),
-//   vec2(0.75, 0.75),
-//   vec2(0.75, 0.0),
-//   vec2(0.75, -0.38),
-//   vec2(0.75, -0.75),
-//   vec2(-0.75, -0.75),
-//   vec2(-0.75, -0.38),
-//   vec2(-0.75, 0.0),
-// ];
-
-// // Load the U into the GPU
-// var vBufferU = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, vBufferU);
-// gl.bufferData(gl.ARRAY_BUFFER, flatten(U), gl.STATIC_DRAW);
-
-// // Associate out shader variables with our data buffer
-// var upositionLoc = gl.getAttribLocation(program, "uPosition");
-// gl.vertexAttribPointer(upositionLoc, 2, gl.FLOAT, false, 0, 0);
-// gl.enableVertexAttribArray(upositionLoc);
-// tLoc = gl.getUniformLocation(program, "t");
-// colorLoc = gl.getUniformLocation(program, "inColor");
-
-// Initialize event handlers
-document.getElementById("Morph").onclick = function () {
-  morph = !morph;
-};
-window.onkeydown = function (event) {
-  var key = String.fromCharCode(event.keyCode);
-  switch (key) {
-    case "1":
-      morph = !morph;
-      break;
-    case "2":
-      deltaT /= 2.0;
-      break;
-    case "3":
-      deltaT *= 2.0;
-      break;
-  }
-};
-render();
-
 function render() {
-  // gl.clear(gl.COLOR_BUFFER_BIT);
-  // if (morph) tParam += deltaT;
-  // if (tParam >= 1.0 || tParam <= 0.0) deltaT = -deltaT;
-  // gl.uniform1f(tLoc, tParam);
-  // color = mix(Icolor, Ucolor, tParam);
-  // gl.uniform4fv(colorLoc, color);
-  // gl.drawArrays(gl.LINE_LOOP, 0, 12);
-  // setTimeout(function () {
-  //   requestAnimationFrame(render);
-  // }, delay);
-
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   theta[axis] += rotatenum;
   gl.uniform3fv(thetaLoc, theta);
 
   gl.drawArrays(gl.TRIANGLES, 0, numPositions);
-  gl.drawArrays(gl.LINES, numPositions, 6);
+  //gl.drawArrays(gl.LINES, numPositions, 6);
   requestAnimationFrame(render);
 }
