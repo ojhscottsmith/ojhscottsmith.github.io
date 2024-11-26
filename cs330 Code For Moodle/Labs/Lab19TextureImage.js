9cd70884f36b30cbaca448968f53c46e0f070f16
ojhscottsmith <ojhscottsmith@gmail.com>
2024-11-25 22:00:12 -0500

    FinalProject

--- cs330 Code For Moodle/Labs/Lab19TextureImage.js
+++ cs330 Code For Moodle/Labs/Lab19TextureImage.js
@@ -15,25 +15,7 @@ var texture;
 var program;
 
 var texSize = 64;
-/*
-// Create a checkerboard pattern using floats
-var image1 = new Array()
-    for (var i =0; i<texSize; i++)  image1[i] = new Array();
-    for (var i =0; i<texSize; i++)
-        for ( var j = 0; j < texSize; j++)
-           image1[i][j] = new Float32Array(4);
-    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
-        var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0));
-        image1[i][j] = [c, c, c, 1];
-    }
 
-// Convert floats to ubytes for texture
-var image2 = new Uint8Array(4*texSize*texSize);
-    for (var i = 0; i < texSize; i++)
-        for (var j = 0; j < texSize; j++)
-           for(var k =0; k<4; k++)
-                image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
-*/
 var texCoordsArray = [];
 
 // perpendicular texture w/isoceles triangle
